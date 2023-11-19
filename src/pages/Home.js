3e8a908225581fe3bc876ee3c8375b2c43bcccf2
AhmadSerhall <ahmad.nour.serhal@gmail.com>
2023-11-19 21:35:26 +0200

    working on the home screen

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react' //must download extension es7 to be able to shortcut component
+import Header from "../components/Header/Header";
+const Home = () => {
+  return (
+    <>
+      <Header/>
+      {/* tabComponent */}
+    </>   
+  );
+};
+
+export default Home;
