7fb4fcc444bae411c5e324a720ebb29988654b21
MarcDagher <marcdagher47@gmail.com>
2023-11-20 10:26:00 +0200

    Finished receiving data from fetch in Movies

--- src/Components/Movies/Movies.js
+++ src/Components/Movies/Movies.js
@@ -6,7 +6,6 @@ export default function Movies(){
     const [movies, setMovies] = useState({})
 
     useEffect(() =>{
-
         // call the imported fetchMovies
         // the return value is an object key(movie genre) : value(fetched object)
         const fechdata = async() =>{
@@ -20,7 +19,6 @@ export default function Movies(){
                 console.log(error)
             }
 
-
             try{
                 const horrorMoviesList = await fetchHorrorMovies();
                 mapMovies.set('horror', horrorMoviesList)
@@ -29,7 +27,6 @@ export default function Movies(){
                 console.log(error)
             }
 
-
             try{
                 const comedyMoviesList = await fetchComedyMovies();
                mapMovies.set('comedy', comedyMoviesList)
@@ -37,16 +34,28 @@ export default function Movies(){
             catch(error){
                 console.log(error)
             }
-            setMovies(mapMovies);
+
+            setMovies(mapMovies)
         }
-        
-        // change movies(useState) to an object mapMovies with the movie key:value
+        // change movies(useState) to an object with key:value
         fechdata()
     },[])
-    console.group(movies)
+    
+    let actionMovies 
+    let horrorMovies
+    let comedyMovies
+
+    if (movies.size) {
+         actionMovies = movies.get('action')
+         horrorMovies = movies.get('horror')
+         comedyMovies = movies.get('comedy')
+    }
+
     return(
         <>
-            <Slide />
+            {actionMovies && <Slide arrOfMovies = {actionMovies} />} 
+            {horrorMovies && <Slide arrOfMovies = {horrorMovies} />}
+            {comedyMovies && <Slide arrOfMovies = {comedyMovies} />}       
         </>
     )
 
