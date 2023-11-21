e91b23e519a8a552bec0ee74d9538f2dd79f3c25
MarcDagher <marcdagher47@gmail.com>
2023-11-21 22:50:59 +0200

    Clean slide code

--- src/Components/movies slide/slide.js
+++ src/Components/movies slide/slide.js
@@ -8,32 +8,6 @@ function Slide({arrOfMovies, genre}){
   const sliderRef = useRef(null)
   let [translateValue, setTranslateValue] = useState(0)
 
-  useEffect(() => {
-    sliderRef.current.innerHTML = ''
-
-    arrOfMovies.forEach(movie =>{
-      const slider_inner_content = document.createElement("div")
-      slider_inner_content.className = 'slider-inner-content'
-      
-      const slider_img_container = document.createElement('div')
-      slider_img_container.className = 'slider-img-container'
-
-        
-      const slider_img = document.createElement('img')
-      slider_img.src = "https://image.tmdb.org/t/p/w500/" + movie.backdrop_path // location of image should be image url direction
-      slider_img.alt = 'movie'
-
-      const slider_p = document.createElement('p')
-      slider_p.textContent = movie.title // p text should be image title
-
-      slider_inner_content.appendChild(slider_img_container)
-      slider_inner_content.appendChild(slider_p)
-      slider_img_container.appendChild(slider_img)
-
-      sliderRef.current.appendChild(slider_inner_content)
-    })
-    
-  })
 
   function slide_left (){
     const  slider = sliderRef.current
@@ -57,16 +31,27 @@ function Slide({arrOfMovies, genre}){
   }
 }
 
-
+const img_url = "https://image.tmdb.org/t/p/w500/"
+console.log(arrOfMovies)
 return <>
+
         <div className='slider-container-spans'>   
             <div className="slider-container">
                 <p>{genre}</p>
                 <span id='span-left' className='span-left' onClick={slide_left}> {left}</span>
                 <div className="slider" ref={sliderRef}>
 
-                  {/* js forEach */}
-                  
+                  {arrOfMovies && (arrOfMovies.map(movie => 
+                      <div className = 'slider-inner-content'>
+                            <div key = {movie.id}>
+                              <div className = 'slider-img-container'>
+                                  <img src= {img_url + movie.backdrop_path} alt='movie'></img>
+                              </div>
+                              <p>{movie.title}</p>
+                            </div>
+                      </div>
+                  ))}
+
                 </div>
                 <span id='span-right' className='span-right' onClick={slide_right}> {right} </span>
             </div>
