0291feee8fb4bf1568eb453253adb1b5b041190f
MarcDagher <marcdagher47@gmail.com>
2023-11-22 20:00:55 +0200

    Initial merge with ahmad

--- src/App.js
+++ src/App.js
@@ -1,3 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles/App.css';
+import Home from './pages/Home';
+import NetFlixShow from './pages/NetFlixShow';
+import Movies from './pages/Movies/Movies';
+import Navbar from './components/common/Navbar/Navbar';
+import MovieDetails from './pages/MovieDetails/MovieDetails';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+
+function App() {
+  return (
+    <div className='entry'>
+      <Navbar />
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Movies />} path="/" />
+          <Route element={<MovieDetails />} path="/movie/:id" />
+          {/* <Route path="/home" element={<Home />} />  */}
+          {/* <Route path="/netflix-show" element={<NetFlixShow/>} /> */}
+        </Routes>
+      </BrowserRouter>
+      </div>
+  );
+}
+
+export default App;
